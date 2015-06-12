e0c511695fe48109f6fb2c5ae9b44456831e4bac
dhleong <falcone88@gmail.com>
2015-06-11 21:11:59 -0400

    Clean up unnecessary setup fields; support multiple devices!
    It looks like Echo is using the UDN field to identify
    devices uniquely; we go ahead and use a "serial" number
    in a couple of places for simplicity. Now just to handle
    the POST!

--- test-discover.js
+++ test-discover.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-// var wemore = require('./');
-// wemore.Discover()
-// .on('device', function(device) {
-//     if (device.friendlyName == 'television' 
-//             || device.friendlyName == 'playstation') {
-//         console.log(device);
-//     }
-// });
-
-var SsdpClient = require('node-ssdp').Client
-  // , BELKIN_CONTROLLEE = "urn:Belkin:device:controllee:1"
-  , BELKIN_CONTROLLEE = "urn:Belkin:device:**"
-  ;
-var client = new SsdpClient({logLevel: 'TRACE'});
-client.on('response', function(resp) {
-    console.log(resp);
+var wemore = require('./');
+wemore.Discover()
+.on('device', function(device) {
+    // if (device.friendlyName == 'Television' 
+    //         || device.friendlyName == 'PS4') {
+        console.log(device);
+    // }
 });
-client.search(BELKIN_CONTROLLEE);
+
+// var SsdpClient = require('node-ssdp').Client
+//   // , BELKIN_CONTROLLEE = "urn:Belkin:device:controllee:1"
+//   , BELKIN_CONTROLLEE = "urn:Belkin:device:**"
+//   ;
+// var client = new SsdpClient({logLevel: 'TRACE'});
+// client.on('response', function(resp) {
+//     console.log(resp);
+// });
+// client.search(BELKIN_CONTROLLEE);
--- test.js
+++ test.js
@@ -2,11 +2,18 @@
 
 var wemore = require('./');
 
-// wemore.Discover()
-// .on('device', function(device) {
-//     console.log(device);
-// });
+var ps4 = wemore.Emulate({friendlyName: "PS4"});
+var tv = wemore.Emulate({friendlyName: "Television"});
 
-// wemore.Emulate({friendlyName: "PS4"});
-wemore.Emulate({friendlyName: "Television"});
+ps4.on('on', function() {
+    console.log("Turn on the PS4!!!");
+});
 
+
+tv.on('off', function() {
+    console.log("Turn off the TV!!!");
+});
+
+tv.on('listening', function() {
+    console.log("TV ready!");
+});
